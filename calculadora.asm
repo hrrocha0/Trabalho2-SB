421923c9d672b6fd11aab0f543954dfe47f9a85b
hrrocha0 <99548553+hrrocha0@users.noreply.github.com>
2023-11-21 16:09:45 -0300

    calculadora: criadas funções 'print' e 'exit'

--- calculadora.asm
+++ calculadora.asm
@@ -6,12 +6,25 @@ len		equ		$ - msg
 
 section	.text
 _start:
-		mov		eax, 4		; sys_write
-		mov		ebx, 1		; stdout
-		mov		ecx, msg	; address
-		mov		edx, len	; length
-		int		80h			; syscall
+		push	msg
+		push	len
+		call	print
 
-		mov		eax, 1		; sys_exit
-		mov		ebx, 0		; code
-		int		80h
+		push	0
+		call	exit
+
+print:	enter	0, 0			; create stack frame
+		mov		eax, 4			; sys_write
+		mov		ebx, 1			; stdout
+		mov		ecx, [ebp + 12]	; buffer
+		mov		edx, [ebp + 8]	; size
+		int		80h				; syscall
+		leave					; destroy stack frame
+		ret		8				; return and clear parameters
+
+exit:	enter	0, 0			; create stack frame
+		mov		eax, 1			; sys_exit
+		mov		ebx, [ebp + 8]	; code
+		int		80h				; syscall
+		leave					; destroy stack frame
+		ret		4				; return and clear parameters
